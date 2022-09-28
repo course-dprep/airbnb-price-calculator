8814657b98c4c43eb80ad15faf246b540c3a3f3c
casrooij <48916882+casrooij@users.noreply.github.com>
2022-09-28 11:44:17 +0200

    New scripts

--- src/data-preparation/download_data_R.R
+++ src/data-preparation/download_data_R.R
@@ -1,7 +1,8 @@
-# load these packages
 library('tidyverse')
 library('dplyr')
-Airbnb_listing_urls <- read.delim("C:/Users/casro/Airbnb_listing_urls.csv", sep = ';') #~ your own wd
+
+Airbnb_listing_urls <- read.delim("Airbnb_listing_urls.csv", sep = ';', encoding='UTF-8')
+
 
 #filter for all cities in EU
 countries_in_EU <- c('Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic', 'Denmark', 
@@ -10,30 +11,23 @@ countries_in_EU <- c('Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of C
 
 #Delete white space of country
 tmp <- sub('.', '', Airbnb_listing_urls$Country)
-
 #Add the countries without the whitespace to df
 Airbnb_listing_urls$Country <- tmp
-
 #Filter for countries in EU
 listing_EU <- Airbnb_listing_urls %>% filter(Country %in% countries_in_EU)
 
-## some cities give an error because of their spelling:
+#Change characters in order to prevent error while downloading data
+listing_EU$Link=gsub('é', '%C3%A9', listing_EU$Link)
+listing_EU$Link=gsub('í', '%C3%AD', listing_EU$Link)
+listing_EU$Link=gsub('ä', '%C3%A4', listing_EU$Link)
+listing_EU$Link=gsub('ü', '%C3%BC', listing_EU$Link)
 
-#Malaga 
-listing_EU$Link[listing_EU$Link=='http://data.insideairbnb.com/spain/andalucía/malaga/2022-06-29/data/listings.csv.gz'] <- 'http://data.insideairbnb.com/spain/andaluc%C3%ADa/malaga/2022-06-29/data/listings.csv.gz'
-#Pays Basque
-listing_EU$Link[listing_EU$Link=='http://data.insideairbnb.com/france/pyrénées-atlantiques/pays-basque/2022-06-10/data/listings.csv.gz'] <- 'http://data.insideairbnb.com/france/pyr%C3%A9n%C3%A9es-atlantiques/pays-basque/2022-06-10/data/listings.csv.gz'
-#Sevilla
-listing_EU$Link[listing_EU$Link=='http://data.insideairbnb.com/spain/andalucía/sevilla/2022-06-27/data/listings.csv.gz'] <- 'http://data.insideairbnb.com/spain/andaluc%C3%ADa/sevilla/2022-06-27/data/listings.csv.gz'
-#stockholm
-listing_EU$Link[listing_EU$Link=='http://data.insideairbnb.com/sweden/stockholms-län/stockholm/2022-06-25/data/listings.csv.gz'] <- 'http://data.insideairbnb.com/sweden/stockholms-l%C3%A4n/stockholm/2022-06-25/data/listings.csv.gz'
-#Trentino
-listing_EU$Link[listing_EU$Link=='http://data.insideairbnb.com/italy/trentino-alto-adige-südtirol/trentino/2022-06-29/data/listings.csv.gz'] <- 'http://data.insideairbnb.com/italy/trentino-alto-adige-s%C3%BCdtirol/trentino/2022-06-29/data/listings.csv.gz'
 
-# making a loop to download all the data links of the EU cities
+dir.create('cities')
+
 for (i in 1:42) { 
-    myurl <- paste(listing_EU[i,3], sep = "") #you need the exact column number so change 1 to that value and change df to your dataframe's name
-    myfile <- paste0("C:/Users/casro/", listing_EU$City[i], ".csv") #~ your own wd
+    myurl <- paste(listing_EU[i,3], sep = "") 
+    myfile <- paste0("cities/", listing_EU$City[i], ".csv")
     download.file(url = myurl, destfile = myfile)
 }
 
