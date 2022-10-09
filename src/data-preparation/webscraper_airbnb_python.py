c257f073d8a47dfa3cf8d46aaf5b6d79ffb525fa
jesperkrauth <j.krauth@tilburguniversity.edu>
2022-10-09 17:25:21 +0200

    add print statements to python scraper

--- src/data-preparation/webscraper_airbnb_python.py
+++ src/data-preparation/webscraper_airbnb_python.py
@@ -1,6 +1,6 @@
 # # Python webscraper Inside Airbnb
-# This webscraper is build to extract all the "listing.csv.gz" download links from Inside Airbnb at one. 
-# These different download links are saved into a csv that is the basis to download all the data in R Studio. 
+# This webscraper is built to extract all the "listing.csv.gz" download links from Inside Airbnb at one. 
+# These different download links are saved into a CSV file that is the basis to download all the data in R Studio. 
 # 
 
 # ### 1) Import the packages
@@ -8,6 +8,8 @@
 from bs4 import BeautifulSoup
 from time import sleep
 import pandas as pd
+import time
+import datetime
 
 
 # ### 2) Prepare Selenium
@@ -18,14 +20,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-inside_airbnb_url= "http://insideairbnb.com/get-the-data.html"
-driver.get(inside_airbnb_url)
-
-
 # ### 3) set up the url to scrape
 
 inside_airbnb_url= "http://insideairbnb.com/get-the-data.html"
 driver.get(inside_airbnb_url)
+print(f"Opening {inside_airbnb_url}")
 
 
 # ### 4) Load the page source and open it with BeautifulSoup
@@ -39,7 +38,9 @@ identifier_list=soup.find_all('h3') # Create a list that provides the identifier
 # ### 5) The loop to get data from all the EU cities
 
 city_data=[] # Creating a list to store the data in
+city_counter = 0 # Counter for print statement
 for city in range(len(cities_list)):
+    city_counter += 1
     tmp_url = cities_list[city].find("a").get('href') # Open the first "a" in each 'tbody' (since the listings.csv.gz file is here) and only extract the 'href' (which is the link of the csv.gz file)
     identifier = identifier_list[city].text # Get the identifier that corresponds to this city (i.e. the city, state/province, country variable)
     country = identifier.split(",",3)[-1] # Save the country, which is always the last part of the identifier that seperates city, state/province and country by comma's.
@@ -47,6 +48,7 @@ for city in range(len(cities_list)):
     
     # Store the city, country and URL link in a temporary dictionary:
     city_info = {"Country": country, "City": city ,"Link": tmp_url}
+    print(f"Saving download URL for {city}, {country} ({city_counter}/{len(cities_list)})")
     
     # Append the temporary dictionary to the list of all cities:
     city_data.append(city_info) 
@@ -54,6 +56,7 @@ for city in range(len(cities_list)):
 
 # ### 6) Save the data to a csv
 
-(pd.DataFrame(city_data)).to_csv("Airbnb_listing_urls.csv", index=False, sep=';') 
+(pd.DataFrame(city_data)).to_csv("Airbnb_listing_urls.csv", index=False, sep=';')
+print("Saved all download URLs to src/data-preparation/airbnb_listing_urls.csv")
 
 
