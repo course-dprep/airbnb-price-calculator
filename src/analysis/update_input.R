7bdcb62211c33de0ff5759880e070f28bb455497
Patrick <112653244+Patrickdeg@users.noreply.github.com>
2022-10-10 11:31:17 +0200

    Delete update_input.R

--- src/analysis/update_input.R
+++
@@ -1,4 +0,0 @@
-# Copy output from data-preparation into input folder 
-# This step really depends no how files are shared across the different stages (e.g. if whole pipeline
-# is on a single machine, could directly access output folder from prepaaration stage)
-file.copy("./gen/data-preparation/output/data_cleaned.RData","./gen/analysis/input/data_cleaned.RData")
