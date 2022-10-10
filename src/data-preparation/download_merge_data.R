388acf364153c245e19f799f96469e638ff77ca4
Patrick <112653244+Patrickdeg@users.noreply.github.com>
2022-10-10 11:44:03 +0200

    Update download_merge_data.R
    made everything lower caps and added nrow to loop

--- src/data-preparation/download_merge_data.R
+++ src/data-preparation/download_merge_data.R
@@ -10,41 +10,41 @@ drive_deauth()
 # Loading the URLS from Google drive
 data_id <-"https://docs.google.com/spreadsheets/d/16pAErs8l2_aAOdhUtmBoY1PIgiPDiQfU/edit?usp=sharing&ouid=117401560079139801880&rtpof=true&sd=true"
 drive_download(as_id(data_id), path = "../../data/Airbnb_listing_urls.xlsx", overwrite = TRUE)
-Airbnb_listing_urls <- read_excel("../../data/Airbnb_listing_urls.xlsx")
+airbnb_listing_urls <- read_excel("../../data/Airbnb_listing_urls.xlsx")
 
 # Filter for all countries in western EU
-countries_in_EU <- c('Belgium', 'France', 'Germany', 'The Netherlands', 'Portugal', 'Spain')
+countries_in_eu <- c('Belgium', 'France', 'Germany', 'The Netherlands', 'Portugal', 'Spain')
 
 # Filter for countries in EU
-listing_EU <- Airbnb_listing_urls %>% filter(Country %in% countries_in_EU)
+listing_eu <- airbnb_listing_urls %>% filter(Country %in% countries_in_eu)
 
 # Create a directory for cities
 dir.create('../../data/cities')
 
 # Subsetting for the specified countries
-listing_EU <- Airbnb_listing_urls %>% subset(Country %in% countries_in_EU)
+listing_eu <- airbnb_listing_urls %>% subset(Country %in% countries_in_eu)
 
 # Change characters in order to prevent error while downloading data
-listing_EU$Link=gsub('√≠', '%C3%AD', listing_EU$Link)
-listing_EU$Link=gsub('√©', '%C3%A9', listing_EU$Link)
+listing_eu$Link=gsub('√≠', '%C3%AD', listing_eu$Link)
+listing_eu$Link=gsub('√©', '%C3%A9', listing_eu$Link)
 
 # Download the files
-for (i in 1:23) { 
-  myurl <- paste(listing_EU[i,3], sep = "") 
-  myfile <- paste0("../../data/cities/", listing_EU$City[i], ".csv")
+for (i in 1:nrow(listing_eu) { 
+  myurl <- paste(listing_eu[i,3], sep = "") 
+  myfile <- paste0("../../data/cities/", listing_eu$City[i], ".csv")
   download.file(url = myurl, destfile = myfile)
 }
 
 # Setting path to directory with correct file type 
-EU_data_files <- list.files(path="../../data/cities", pattern=".csv")
+eu_data_files <- list.files(path="../../data/cities", pattern=".csv")
 
 # Read through the csv files and assign columns city & country
 airbnb_dfs <- Map(
   function(data_file, country, city) 
     read.csv(file.path("../../data/cities", data_file)) %>% transform(country=country, city=city),
-  EU_data_files,
-  listing_EU$Country,
-  listing_EU$City
+  eu_data_files,
+  listing_eu$Country,
+  listing_eu$City
   )
 
 # Bind the data together to one dataframe
